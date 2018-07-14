3e79455eee602770a0b571967e0924781eb03cd8
우아테크캠프_1 <woowatechcamp_1@woowatechcamp1.local>
2018-07-14 21:18:06 +0900

    feat: Initialize project
    and add api for hello world :)

---
+++ src/main/java/org/junsulime/assistant/api/HelloController.java
@@ -0,0 +1,15 @@
+package org.junsulime.assistant.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
